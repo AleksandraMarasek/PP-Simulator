f28be118204eac787e54334aee266f4cab34686d
Aleksandra Marasek <232418@student.uek.krakow.pl>
2024-10-26 00:47:08 +0200

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    //public string Info => $"{Name} [{Level}]";
+    public Creature()
+    {
+        Name = "Unknown";
+        Level = 1;
+    }
+    
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+
+    public void Info() => Console.WriteLine($"{Name} [{Level}]");
+
+
+
+
+}
+
